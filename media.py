9a5a335434317b0e08e1abcbfdbd46bd84a13805
nate421 <greg_julie@roadrunner.com>
2016-11-10 15:33:42 -0800

    firstcommit

---
+++ media.py
@@ -0,0 +1,7 @@
+import movie
+
+toy_story = media.Movie("Toy Story",
+"A story of a boy and his toys that come to life",
+"https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
+"https://www.youtube.com/watch?v=tN1A2mVnrOM")
+print(toy_story.storyline)
---
+++ movie.py
@@ -0,0 +1,6 @@
+class Movie()
+    def __init__(self title, storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
