3afd836ed2f7743382c52ab044911a091249ea91
nate421 <greg_julie@roadrunner.com>
2016-11-14 14:35:27 -0800

    avatar

--- media.py
+++ media.py
@@ -4,4 +4,9 @@ toy_story = media.Movie("Toy Story",
 "A story of a boy and his toys that come to life",
 "https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
 "https://www.youtube.com/watch?v=tN1A2mVnrOM")
-print(toy_story.storyline)
+# print(toy_story.storyline)
+avatar = media.Movie("Avatar",
+                    "A marine on a alien planet",
+                    "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+                    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+print(avatar.storyline)
