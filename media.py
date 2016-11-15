bfeaf521305ee352cac745d0a321f8f4ab7dd40d
nate421 <greg_julie@roadrunner.com>
2016-11-15 14:11:07 -0800

    fix first

--- media.py
+++ media.py
@@ -1,12 +1,13 @@
-import movie
+import move
 
-toy_story = media.Movie("Toy Story",
+toy_story = move.Movie("Toy Story",
 "A story of a boy and his toys that come to life",
 "https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
 "https://www.youtube.com/watch?v=tN1A2mVnrOM")
 # print(toy_story.storyline)
-avatar = media.Movie("Avatar",
+avatar = move.Movie("Avatar",
                     "A marine on a alien planet",
                     "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 print(avatar.storyline)
+avatar.show_trailer()
