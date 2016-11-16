cdf7202ea9afae978f4f745f24d1d9f17d1ba9b9
nate421 <greg_julie@roadrunner.com>
2016-11-16 13:31:51 -0800

    finshed product

--- media.py
+++ media.py
@@ -1,13 +1,21 @@
-import move
+import fresh_tomatoes
+import movie
 
-toy_story = move.Movie("Toy Story",
-"A story of a boy and his toys that come to life",
-"https://en.wikipedia.org/wiki/File:Toy_Story.jpg",
-"https://www.youtube.com/watch?v=tN1A2mVnrOM")
+toy_story = movie.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://www.impawards.com/1995/posters/toy_story_ver1.jpg",
+                        "https://www.youtube.com/watch?v=tN1A2mVnrOM")
 # print(toy_story.storyline)
-avatar = move.Movie("Avatar",
+avatar = movie.Movie("Avatar",
                     "A marine on a alien planet",
-                    "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg",
+                    "http://www.impawards.com/2009/posters/avatar.jpg",
                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
-print(avatar.storyline)
-avatar.show_trailer()
+# print(avatar.storyline)
+# avatar.show_trailer()
+enders_game = movie.Movie("Ender`s game",
+                          "A kid that leaves earth to train for battle vs a ailen army",
+                          "https://upload.wikimedia.org/wikipedia/en/8/8c/Ender's_Game_poster.jpg",
+                          "https://www.youtube.com/watch?v=vP0cUBi4hwE")
+
+movies = [toy_story, avatar, enders_game]
+fresh_tomatoes.open_movies_page(movies)
