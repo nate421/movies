c7481513ea9b948a206bb04751334f04f945017e
nate421 <greg_julie@roadrunner.com>
2016-12-15 10:00:52 -0800

    Final Change

--- inherit.py
+++ inherit.py
@@ -14,6 +14,11 @@ class Child(Parent):
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color - "+self.eye_color)
+        print("Number of toys - "+str(self.number_of_toys))
+
 billy_cyrus = Parent("cyrus", "green")
 billy_cyrus.show_info()
 miley_cyrus = Child("cyrus", "green", 5)
